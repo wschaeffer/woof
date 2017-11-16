ba1a6ad74d45007bdb0ca83bab81f7d50ab8b742
wschaeffer <w.schaeffer@student.fontys.nl>
2017-11-16 10:41:57 +0100

    Clean up

--- src/main.cpp
+++ src/main.cpp
@@ -13,10 +13,15 @@
 #define PROXIMITY_DEADZONE 3
 #define PROXIMITY_TOUCHED 30
 
-LEDstrip led          = LEDstrip( PIN_DATA, PIN_CLOCK, LEDCOUNT );
-
-uint16_t  refA0;
-rgb_color colorOrange = {255, 25, 0, 1};
+bool      touched        = false;
+uint8_t   position       = 0;
+uint8_t   positionTarget = 0;
+uint8_t   maxLed         = 0;
+uint8_t   proximity      = 0;
+uint16_t  speed          = 0;
+uint16_t  refA0          = 0;
+rgb_color colorOrange    = {255, 25, 0, 1};
+LEDstrip  led            = LEDstrip( PIN_DATA, PIN_CLOCK, LEDCOUNT );
 
 void setup()
 {
@@ -27,13 +32,6 @@ void setup()
 
 void loop()
 {
-    static bool     touched        = false;
-    static uint8_t  position       = 0;
-    static uint8_t  positionTarget = 0;
-    static uint8_t  maxLed         = 0;
-    static uint8_t  proximity      = 0;
-    static uint16_t speed          = 0;
-
     if ( position == positionTarget )
     {
         proximity = ADCTouch.read( A0 ) - refA0;
@@ -54,14 +52,7 @@ void loop()
         speed          = ( uint16_t ) analogRead( 0 ) / ( 1023 / 50 );
     }
 
-    if ( position < positionTarget )
-    {
-        position++;
-    }
-    else
-    {
-        position--;
-    }
+    position < positionTarget ? ( position++ ) : ( position-- );
 
     if ( proximity )
     {
